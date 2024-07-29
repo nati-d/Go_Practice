8a57648762b97fe0b38ec341ce9594a00d7ebbdf
Nathnael Desalegn <nathnaeldes@gmail.com>
2024-07-30 00:26:19 +0300

    task two in the same folder

--- Word_Counter/app.go
+++ Word_Counter/app.go
@@ -8,26 +8,37 @@ import (
 )
 
 func main() {
-
+	fmt.Println("What Do You Want To Do?")
+	fmt.Println("1. Count Words")
+	fmt.Println("2. Check Palindrome")
+	var choice int
+	fmt.Print("Enter Your Choice: ")
+	fmt.Scanln(&choice)
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter a sentence: ")
+	fmt.Print("Enter a word: ")
 	sentence, _ := reader.ReadString('\n')
-
 	sentence = trimWord(sentence)
 	sentence = toLowerCase(sentence)
 	sentence = removePunctuation(sentence)
 
-	wordCount := wordCounter(sentence)
+	if choice == 1{
+		wordCount := wordCounter(sentence)
 
-
-	fmt.Println("Word Count: ", wordCount)
+		fmt.Println("Word Count: ", wordCount)
+	}else if choice == 2{
+		if isPalindrome(sentence) {
+			fmt.Println("The word " ,sentence, " is a palindrome.")
+		} else {
+			fmt.Println("The word " ,sentence, " is not palindrome.")
+		}
+	}
 
 }
 
 func removePunctuation(word string) string {
 	var result string
 	for i := 0; i < len(word); i++ {
-		if int(word[i]) >= 65 && int(word[i]) <= 90 || int(word[i]) >= 97 && int(word[i]) <= 122 {
+		if string(word[i]) == " " || int(word[i]) >= 65 && int(word[i]) <= 90 || int(word[i]) >= 97 && int(word[i]) <= 122 {
 			result += string(word[i])
 		}
 	}
@@ -54,4 +65,14 @@ func wordCounter(sentence string) map[string]int {
 		dct[word]++
 	}
 	return dct
+}
+
+func isPalindrome(word string) bool {
+	var reversedWord string
+
+	for i := len(word) - 1; i >= 0; i-- {
+		reversedWord += string(word[i])
+	}
+
+	return word == reversedWord
 }

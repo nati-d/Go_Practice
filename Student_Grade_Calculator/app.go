e92607b847178beb3b873a50e3e528adc9828918
Nathnael Desalegn <nathnaeldes@gmail.com>
2024-07-30 10:35:14 +0300

    updated Grade calculator

--- Student_Grade_Calculator/app.go
+++ Student_Grade_Calculator/app.go
@@ -3,51 +3,68 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/rodaine/table"
 	"os"
+	"strings"
 )
 
-
-
 func main() {
-	fmt.Print("Enter Your Name: ")
-	var name = acceptInput()
-	fmt.Print("Enter Your Number of Subjects: ")
-	var numberOfSubjects float64
-	fmt.Scanf("%f", &numberOfSubjects)
+	fmt.Println("------------------------------------------------------------")
+	fmt.Println("          Welcome to the Grade Calculator!                  ")
+	fmt.Println("------------------------------------------------------------")
 
-	var dct = make(map[string]float64)
-	for i := 0; i < int(numberOfSubjects); i++ {
+	fmt.Print("Please enter your name: ")
+	name := strings.TrimSpace(acceptInput())
+
+	var numberOfSubjects int
+	for {
+		fmt.Print("How many subjects do you have, ", name, "? ")
+		fmt.Scanf("%d", &numberOfSubjects)
+		if numberOfSubjects > 0 {
+			break
+		} else {
+			fmt.Println("Please enter a valid number of subjects (greater than 0).")
+		}
+	}
+
+	dct := make(map[string]float64)
+	for i := 0; i < numberOfSubjects; i++ {
 		var subject string
 		var marks float64
 
-		fmt.Print("Enter Subject ", i+1, " Name: ")
-		fmt.Scanf("%s ", &subject)
-		fmt.Print("Enter Marks: ")
-		fmt.Scanf("%f", &marks)
+		fmt.Printf("Enter the name of subject %d: ", i+1)
+		fmt.Scanf("%s", &subject)
 
 		for {
-
-			if marks < 0 || marks > 100 {
-				fmt.Println("Enter Valid Grades that is greater than or equals 0 and less than or equals 100")
-				fmt.Print("Enter Valid Mark for ", subject, ": ")
-				fmt.Scanf("%f", &marks)
-			} else {
+			fmt.Printf("Enter your marks for %s (0-100): ", subject)
+			fmt.Scanf("%f", &marks)
+			if marks >= 0 && marks <= 100 {
 				break
+			} else {
+				fmt.Println("Invalid marks! Please enter a value between 0 and 100.")
 			}
 		}
 
 		dct[subject] = marks
 	}
+	buildTable(dct, name)
+	fmt.Println("------------------------------------------------------------")
+	var message string
+	if calculateAverage(dct) >= 50 {
+		message = "Congratulations! You have passed."
+	} else {
+		message = "Sorry! You have failed."
+	}
+	fmt.Printf("Your average grade is %.2f\n", calculateAverage(dct))
+	fmt.Println(message)
 
-	average := calculateAverage(dct)
-	fmt.Println("Dear ", name, " Your Total Average Score is: ", average)
 }
 
-func acceptInput() string{
+func acceptInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	word, _ := reader.ReadString('\n')
-
-	return word
+	return strings.TrimSpace(word)
 }
 
 func calculateAverage(dct map[string]float64) float64 {
@@ -56,4 +73,21 @@ func calculateAverage(dct map[string]float64) float64 {
 		sumOfMarks += value
 	}
 	return sumOfMarks / float64(len(dct))
+}
+
+func buildTable(dct map[string]float64, name string) {
+	average := calculateAverage(dct)
+
+	fmt.Printf("\n%s's Grade Report\n", name)
+	tbl := table.New("ID", "Subject", "Mark")
+	id := 1
+	for key, value := range dct {
+		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+		columnFmt := color.New(color.FgYellow).SprintfFunc()
+		tbl.AddRow(id, key, value)
+		tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt).WithPadding(6)
+		id++
+	}
+	tbl.AddRow("*", "Average", average)
+	tbl.Print()
 }
--- Student_Grade_Calculator/go.mod
+++ Student_Grade_Calculator/go.mod
@@ -1,3 +1,11 @@
 module example.com/task_one
 
 go 1.22.5
+
+require (
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rodaine/table v1.2.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
